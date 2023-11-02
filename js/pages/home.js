f2da732e312c4f37e18741869d37fb3b06bbbf9d
aaaa-trsh <kieth.tienyou.bartlett@gmail.com>
2023-11-02 00:14:45 -0700

    fix title

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,8 @@
 export default {
     template: `
     <div>
-        <h1 class="main-title no-select">JOVAN<br/>BARTLETT</h1>
+        <h1 class="main-title no-select">JOVAN</h1>
+        <h1 class="main-title no-select">BARTLETT</h1>
     </div>
     `,
 };
